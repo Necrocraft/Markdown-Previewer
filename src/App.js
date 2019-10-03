5623fd1df0538082bbb02e6912675e96b6d63da2
Necrocraft <mhdzeefan@gmail.com>
2019-10-03 15:11:22 +0530

    First commit with all formatting and changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,109 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const marked = require('marked');
+
+const initialMarkdown =`
+### Headers
+
+# Header 1
+## Header 2
+### Header 3
+#### Header 4
+##### Header 5
+###### Header 6
+
+### List
+
+- item one
+- item two
+- item three
+
+### Links
+
+[FreeCodeCamp](https://learn.freecodecamp.org)
+
+[Google](https://google.com 'Most Popular Search engine')
+
+
+### Text Decorations
+
+*italic*
+
+**bold**
+
+***bold and italic***
+
+### Images
+
+![alt text](https://cdn.pixabay.com/photo/2018/04/20/17/18/cat-3336579__340.jpg 'Cute Cat')
+
+### Blockquote
+
+> Honesty is the best policy.
+
+### Code
+
+\`npm install create-react-app -g\`
+
+\`\`\`
+  function addTwoNum(a,b) {
+    return(a + b);
+}
+\`\`\`
+`
+
+var renderer = new marked.Renderer()
+
+renderer.link = function(href, title, text) {
+  return `<a href=${href} target="_blank">${text}</a>`
+}
+marked.setOptions({
+  renderer,
+  breaks: true
+});
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      markdown: initialMarkdown
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.clearText = this.clearText.bind(this);
+  }
+  
+  handleChange(e) {
+    this.setState({
+      markdown: e.target.value
+    })
+  }
+  clearText() {
+    this.setState({
+      markdown: ""
+    })
+  }
+  
+  render() {
+    return(
+    <div>
+      <h1 id="heading">Markdown Previewer</h1><hr />
+        <div className="container">
+        <h2 id="edit">Editor</h2>
+        <h2 id="pre">Previewer</h2>
+        </div> 
+        <div className="container">
+          <div className="left-area">
+            <textarea id="editor" value={this.state.markdown} onChange={this.handleChange}/>
+            <button onClick={this.clearText}>Clear Text</button>
+            </div>
+          <div className="right-area">
+            <div id="preview" dangerouslySetInnerHTML={{__html: marked(this.state.markdown)}} />
+          </div>
+        </div>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
